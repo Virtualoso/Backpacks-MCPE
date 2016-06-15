ec8ff83203d939309fc3aab02dca7cf54d48c4f7
TrinityDevelopers <trinitydevelopers4christ@gmail.com>
2016-06-15 17:43:15 -0500

    a couple more fixes

--- jni/com/virtualoso/backpacks/gui/BackpackPlayScreen.cpp
+++ jni/com/virtualoso/backpacks/gui/BackpackPlayScreen.cpp
@@ -1,6 +1,9 @@
 #include "BackpackPlayScreen.h"
 
+#include "com/mojang/minecraftpe/client/MinecraftClient.h
 #include "com/mojang/minecraftpe/client/renderer/texture/TextureGroup.h"
+#include "com/mojang/minecraftpe/client/gui/IntRectangle.h"
+#include "com/mojang/minecraftpe/client/gui/ImageWithBackground.h"
 
 BackpackPlayScreen::BackpackPlayScreen(MinecraftClient& client) : InGamePlayScreen(client)
 {
@@ -11,9 +14,11 @@ void BackpackPlayScreen::init()
 {
 	if(!backpackButton)
 	{
-	backpackButton = std::make_shared<ImageWithBackground>(2);
-	backpackButton->init(mcClient->getTextures(), 28, 28, {49, 43, 14, 14}, {49, 43, 14, 14}, 2, 2, "gui/spritesheet.png");
-	backpackButton->setImageDef({mcClient->getTextures()->getTexture("gui/spritesheet.png", TextureLocation::Default), 0, 1, 18.0F, 18.0F, {60, 0, 18, 18}, true}, true);
+		backpackButton = std::make_shared<ImageWithBackground>(1);
+		backpackButton->init(mcClient->getTextures(), 28, 28, {49, 43, 14, 14}, {49, 43, 14, 14}, 2, 2, "gui/spritesheet.png");
+		backpackButton->setImageDef({mcClient->getTextures()->getTexture("gui/spritesheet.png", TextureLocation::Default), 0, 1, 18.0F, 18.0F, {60, 0, 18, 18}, true}, true);
+		
+		buttonList.emplace_back(backpackButton);
 	}
 }
 

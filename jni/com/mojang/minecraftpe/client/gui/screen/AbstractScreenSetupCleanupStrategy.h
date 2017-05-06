4800a188a0d4204d3a07b57b38e0b2124bc69697
ThePixelCoder <thepixelmaster2354@gmail.com>
2017-05-05 21:46:25 -0500

    Big overhaul
    "Updated" headers, added headers, and other small things

--- jni/com/mojang/minecraftpe/client/gui/screen/AbstractScreenSetupCleanupStrategy.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class AbstractScreenSetupCleanupStrategy {
-public:
-	virtual ~AbstractScreenSetupCleanupStrategy();
-	virtual void setupScreen(ScreenContext&);
-	virtual void cleanupScreen(ScreenContext&);
-	virtual int getNumberOfRenderPasses() const;
-};
